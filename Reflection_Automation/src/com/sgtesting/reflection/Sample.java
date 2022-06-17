326a29f5958efc416b473155eb6e7f8728fcdfc3
HanamantVikas <hanamant0504@gmail.com>
2022-06-17 17:45:05 +0530

    Reflection Day1

---
+++ Reflection_Automation/src/com/sgtesting/reflection/Sample.java
@@ -0,0 +1,23 @@
+package com.sgtesting.reflection;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void display1()
+	{
+		System.out.println("dispaly1() Method...");
+	}
+	public void display2()
+	{
+		System.out.println("dispaly2() Method...");
+	}
+
+	public void display3()
+	{
+		System.out.println("dispaly3() Method...");
+	}
+}
